88f586be6cce3a811d26ab9ae331e3dd89e33bd2
jpmllr89 <jpmllr89@gmail.com>
2023-10-03 15:08:11 -0400

    add eventlisteners, trying to figure out how to add active status to buttons

--- js/main.js
+++ js/main.js
@@ -6,6 +6,54 @@ const dark = 'dark';
 const light = 'light';
 const open = 'open';
 const active = '.active';
+const root = document.documentElement;
+
+/* Theme */
+
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
+const setActive = (e, selector) => {
+  if (document.querySelector(`${selector}${active}`) !== null){
+    document.querySelector(`${selector}${active}`).classList.remove(active);
+  }else{
+    e.classList.add(active);
+  }
+}
+
+const setTheme = (val) => {
+
+  if(val === dark){
+    root.setAttribute(dataTheme, dark);
+    localStorage.setItem(theme, dark);
+  }else{
+    root.setAttribute(dataTheme, light);
+    localStorage.setItem(theme, light);
+  }
+}
+
+toggleTheme.addEventListener('click', function(){
+  const tab = this.parentElement.parentElement;
+  if(!tab.className.includes(open)){
+    tab.classList.add(open);
+  }else{
+    tab.classList.remove(open);
+  }
+});
+
+for(const el of switcher){
+  el.addEventListener('click', function(){
+    const toggle = this.dataset.toggle;
+    // set active state
+    setActive(el, switcherBtn);
+    setTheme(toggle);
+
+  });
+}
+
+
+/*Modal*/
 
 const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
