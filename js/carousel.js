5a37708ecdbcc2a575b78bef121c57455de4cf25
jpmllr89 <jpmllr89@gmail.com>
2023-10-12 19:44:19 -0400

    assign event listener to buttons

--- js/carousel.js
+++ js/carousel.js
@@ -5,3 +5,28 @@ let current = Math.floor(Math.random() * slides.length);
 let next = current < slides.length -1 ? current +1 : 0; 
 let prev = current > 0 ? current -1 : slides.length -1;
 
+// create event listener for the buttons
+
+buttons[0].addEventListener('click' e => {
+
+});
+// decide how to call prev/next
+//update variables
+// current index = newIndex
+// nextIndex = current +1 : 0
+//prevIndex = current -1 : slides.length -1
+
+//update css classes
+
+
+const goToNext = () => {
+
+};
+
+const goToPrev = () =>{
+
+}
+
+for(let i in buttons){
+  buttons[i].addEventListener('click', () => i === 0 ? goToPrev() : goToNext());
+}
